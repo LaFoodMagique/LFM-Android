821c7558e45a03861080189b716db0381e82e3d4
Sparkh <sp4rkh@MacBook-Pro-de-Jean-Antoine.local>
2016-05-16 01:24:17 +0800

    Merge branch 'master' into api
    # Conflicts:
    #	.idea/libraries/animated_vector_drawable_23_3_0.xml
    #	.idea/libraries/appcompat_v7_23_3_0.xml
    #	.idea/libraries/support_v4_23_3_0.xml
    #	.idea/libraries/support_vector_drawable_23_3_0.xml
    #	.idea/workspace.xml
    #	app/app.iml
    #	app/build.gradle
    #	boommenu/boommenu.iml
    #	boommenu/build.gradle

---
+++ app/src/main/java/com/sourcey/foodie/Fragments/BasicFragment.java
@@ -0,0 +1,54 @@
+package com.sourcey.foodie.Fragments;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrateur on 15/05/2016.
+ */
+public class BasicFragment extends Fragment {
+
+    protected TabLayout tabLayout;
+    protected ViewPager viewPager;
+
+    public BasicFragment() {
+        // Required empty public constructor
+    }
+
+    protected class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFrag(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/sourcey/foodie/Fragments/TopFragments/ReservationFragment.java
@@ -0,0 +1,56 @@
+package com.sourcey.foodie.Fragments.TopFragments;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sourcey.foodie.Fragments.BasicFragment;
+import com.sourcey.foodie.Fragments.BottomFragments.BlankFragment;
+import com.sourcey.foodie.R;
+
+
+public class ReservationFragment extends BasicFragment {
+
+    private View myFragmentView;
+
+    public ReservationFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        myFragmentView = inflater.inflate(R.layout.fragment_top_home, container, false);
+        viewPager = (ViewPager) myFragmentView.findViewById(R.id.bottom_viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) myFragmentView.findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        return myFragmentView;
+    }
+
+    /**
+     * Adding fragments to ViewPager
+     * @param viewPager
+     */
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getFragmentManager());
+        adapter.addFrag(new BlankFragment(), getString(R.string.tab_reservation_string_first));
+        adapter.addFrag(new BlankFragment(), getString(R.string.tab_reservation_string_second));
+        adapter.addFrag(new BlankFragment(), getString(R.string.tab_reservation_string_third));
+        viewPager.setAdapter(adapter);
+    }
+}

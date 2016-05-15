b3676bae9c4273e9ee9ab26502611368eaf11501
Sparkh <sp4rkh@MacBook-Pro-de-Jean-Antoine.local>
2016-05-16 01:20:20 +0800

    APIRoot amelioration
    create an enum for make easy the api root changes

--- app/src/main/java/com/sourcey/foodie/API/APIManager.java
+++ app/src/main/java/com/sourcey/foodie/API/APIManager.java
@@ -7,18 +7,35 @@ import com.sourcey.foodie.API.managers.Authenticate;
  */
 public class APIManager
 {
-    private final static String rootURL = "http://127.0.0.1:8000/";
+    public enum APIRoot
+    {
+        LOCAL,
+        DEV,
+        PROD;
+
+        @Override
+        public String toString() {
+            switch (this) {
+                case LOCAL:
+                    return "http://127.0.0.1:3000/api";
+                case DEV:
+                    return "http://192.168.169.143:3000/api";
+                case PROD:
+                    return "http://xxx.xxx.xxx.xxx:3000/api";
+            }
+            return null;
+        }
+    }
+
+    private final static APIRoot rootURL = APIRoot.LOCAL;
 
     public static String token = null;
 
-    public static Authenticate auth = new Authenticate(rootURL);
-
+    public static Authenticate auth = new Authenticate(rootURL.toString());
 
 
     public static interface APIListener
     {
         public void onResponse(String json);
     }
-
-
 }

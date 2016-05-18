75ad5737e6f7d864829d51ca5453d05f8ab90e22
Perith <mrpracks@gmail.com>
2016-05-18 21:45:22 +0800

    add fragment and layout

---
+++ app/src/main/java/com/sourcey/foodie/Fragments/BottomFragments/RestoPageFind.java
@@ -0,0 +1,30 @@
+package com.sourcey.foodie.Fragments.BottomFragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sourcey.foodie.R;
+
+/**
+ * Created by Perith on 18/05/2016.
+ */
+public class RestoPageFind extends Fragment {
+    public RestoPageFind() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_resto_page_find, container, false);
+    }
+}

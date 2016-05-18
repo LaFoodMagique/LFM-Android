44905ed4d08b764a3c818c2458aa5c1163be9bd2
Perith <mrpracks@gmail.com>
2016-05-18 22:39:09 +0800

    add fragment & layout

---
+++ app/src/main/java/com/sourcey/foodie/Fragments/BottomFragments/HomeReservation.java
@@ -0,0 +1,30 @@
+package com.sourcey.foodie.Fragments.BottomFragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sourcey.foodie.R;
+
+/**
+ * Created by Perith on 18/05/2016.
+ */
+public class HomeReservation extends Fragment {
+    public HomeReservation() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_reservation, container, false);
+    }
+}
---
+++ app/src/main/java/com/sourcey/foodie/Fragments/BottomFragments/ReservationFutur.java
@@ -0,0 +1,30 @@
+package com.sourcey.foodie.Fragments.BottomFragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sourcey.foodie.R;
+
+/**
+ * Created by Perith on 18/05/2016.
+ */
+public class ReservationFutur extends Fragment {
+    public ReservationFutur() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reservation_futur, container, false);
+    }
+}
